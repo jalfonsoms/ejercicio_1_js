cba8072516ad320d4d8cca87e3a8b2f206a1524a
Jose Mercado <alfonso.mercado@udea.edu.co>
2023-11-13 10:04:12 -0500

    Agregando informacion adicional

--- script.js
+++ script.js
@@ -1,47 +1,86 @@
 const consumeApiWithAxios = async (url) => {
-    try{
+    try {
         const response = await axios.get(url);
-        console.log(`La peticion a la api se completó correctamentne con status: ${response.status}`);
-        return await response.data;
-    } catch (error){
-        console.error(`Falló la peticion a la api con error: ${error.message}`);
+        console.log(`La petición a la API se completó correctamente con status: ${response.status}`);
+        return response.data.results; // Devolver response.data.results en lugar de response.results
+    } catch (error) {
+        console.error(`Falló la petición a la API con error: ${error.message}`);
+        return []; // Devolver un array vacío en caso de error para evitar problemas más adelante
     }
 }
 
-const respuestaPeticion = consumeApiWithAxios('https://reqres.in/api/users?page=2');
-
-procesarDatosRespuesta(respuestaPeticion);
-
-/* Acceder a los datos en la respuesta de la peticion */
-
-async function procesarDatosRespuesta(resp){
-    const respApi = await resp;
-    const datos = respApi.data;
-    const main = document.querySelector('main');
-    const contenedor = document.createElement('div');
-    contenedor.setAttribute('class', 'container');
-    for(dato of datos){
-        const user = document.createElement('div');
-        user.setAttribute('class', 'user');
-        const userImg = document.createElement('div');
-        userImg.setAttribute('class', 'user-pic');
-        const img = document.createElement('img');
-        img.setAttribute('src', dato.avatar);
-        const userName = document.createElement('div');
-        userName.setAttribute('class', 'user-name');
-        const userEmail = document.createElement('div');
-        userEmail.setAttribute('class', 'user-email');
-        const nombreCompleto = document.createElement('p');
-        nombreCompleto.innerHTML = `<strong>Nombre: </strong>${dato.first_name} ${dato.last_name}`;
-        const email = document.createElement('p');
-        email.innerHTML = `<strong>Email: </strong>${dato.email}`;
-        userName.appendChild(nombreCompleto);
-        userEmail.appendChild(email);
-        userImg.appendChild(img);
-        user.appendChild(userImg);
-        user.appendChild(userName);
-        user.appendChild(userEmail);
-        contenedor.appendChild(user);
-        main.appendChild(contenedor);
+async function procesarNombres(resp) {
+    try {
+        const respApi = await resp;
+        const nombres = respApi.map(item => item.name); // Mapea los nombres desde el array de objetos
+        return nombres; // Devuelve los nombres
+    } catch (error) {
+        console.error(`Error al procesar la respuesta: ${error.message}`);
+        return [];
     }
 }
+
+async function obtenerNombresPaginados(offset, limit) {
+    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+    const respuestaPeticion = consumeApiWithAxios(url);
+    return await procesarNombres(respuestaPeticion);
+}
+
+async function perfilPokemon(nombre) {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${nombre.toLowerCase()}`)
+        .then((response) => {
+            const nombrePokemon = document.getElementById('nombrePokemon');
+            const imagenPokemon = document.getElementById('imagenPokemon')
+
+            const pokemonData = response.data;
+
+            nombrePokemon.textContent = `Nombre: ${pokemonData.name}`;
+            imagenPokemon.setAttribute('src', pokemonData.sprites.other["official-artwork"].front_default);
+        })
+        .catch((error) => {
+            const nombrePokemon = document.getElementById('nombrePokemon');
+            nombrePokemon.textContent = `No se encontró ${nombre}. Por favor, verifica el nombre.`;
+        });
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+    const searchInput = document.getElementById("searchInput");
+    const suggestionsList = document.getElementById("suggestionsList");
+    const searchButton = document.querySelector(".search-button");
+
+    searchInput.addEventListener("input", async function () {
+        const inputValue = searchInput.value.toLowerCase();
+
+        // Filtra las sugerencias según el valor de entrada
+        const filteredSuggestions = await obtenerNombresPaginados(0, 1000); // Ajusta el límite según tus necesidades
+        const filteredResults = filteredSuggestions.filter(item => item.toLowerCase().includes(inputValue));
+
+        // Muestra las sugerencias filtradas
+        displaySuggestions(filteredResults);
+    });
+
+    searchButton.addEventListener("click", function () {
+        const inputValue = searchInput.value.toLowerCase();
+        perfilPokemon(inputValue);
+    });
+
+    document.addEventListener("click", function (e) {
+        if (!suggestionsList.contains(e.target)) {
+            suggestionsList.innerHTML = "";
+        }
+    });
+
+    function displaySuggestions(suggestions) {
+        suggestionsList.innerHTML = "";
+
+        suggestions.forEach(function (suggestion) {
+            const listItem = document.createElement("li");
+            listItem.textContent = suggestion;
+            listItem.addEventListener("click", function () {
+                searchInput.value = suggestion;
+                suggestionsList.innerHTML = "";
+            });
+            suggestionsList.appendChild(listItem);
+        });
+    }
+});
